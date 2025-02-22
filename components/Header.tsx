0cc56a41550ab63a548212aa04ccde2101397572
fraolb <Fraolbereket@gmail.com>
2025-02-22 21:31:53 +0300

    check user if creator in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,19 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { FaSearch, FaUserCircle, FaBars } from "react-icons/fa";
-import { CiBitcoin } from "react-icons/ci";
 import { useState, useContext, useEffect } from "react";
 import CirclesSDKContext from "@/app/contexts/CirclesSDK";
 import Image from "next/image";
 import Logo from "@/public/etheraLogo.png";
+import { Menu, CircleUserRound, Bitcoin, Search } from "lucide-react";
+import { Avatar } from "@circles-sdk/sdk";
 
 const Header = () => {
   const router = useRouter();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [userBalance, setUserBalance] = useState("0");
-  const [avatarInfo, setAvatar] = useState(null);
+  const [userBalance, setUserBalance] = useState(0);
+  const [avatarInfo, setAvatar] = useState<Avatar | null>(null);
+  const [isUserCreator, setIsUserCreator] = useState(false);
 
   // Use the Circles SDK context
   const { sdk, circlesAddress, circlesProvider } =
@@ -26,7 +27,7 @@ const Header = () => {
       console.log("avatar ", avatar);
       if (avatar) {
         setAvatar(avatar);
-        const total = await avatar.getTotalBalance(circlesAddress);
+        const total = await avatar.getTotalBalance();
         setUserBalance(total);
       } else {
         setAvatar(null);
@@ -47,8 +48,34 @@ const Header = () => {
     setIsDropdownOpen(false); // Close dropdown after navigation
   };
 
+  const isCreator = async (address: string): Promise<boolean> => {
+    try {
+      // Fetch user data from the [walletAddress] route
+      const response = await fetch(`/api/users/${address}`);
+
+      // If the response is not OK, the user does not exist
+      if (!response.ok) {
+        setIsUserCreator(false);
+        return false;
+      }
+
+      // Parse the response JSON
+      const user = await response.json();
+
+      // Check if the user is a creator
+      setIsUserCreator(user.isCreator);
+      return user.isCreator === true;
+    } catch (error) {
+      console.error("Error checking if user is a creator:", error);
+      throw new Error("Failed to check user status");
+    }
+  };
+
   useEffect(() => {
     getCRCBalance();
+    if (circlesAddress) {
+      isCreator(circlesAddress);
+    }
   }, [circlesAddress, circlesProvider]);
   console.log(avatarInfo);
 
@@ -70,45 +97,48 @@ const Header = () => {
 
         {/* Search bar */}
         <div className="w-2/3 mx-2 md:mx-0 flex justify-center">
-          <div className="w-full relative">
-            <FaSearch className="absolute left-3 top-3 text-gray-400" />
+          <div className="w-2/3 relative">
+            <Search className="absolute left-3 top-2 text-gray-400" />
             <input
               type="text"
               placeholder="Search creators..."
-              className="w-full pl-10 pr-4 py-2 border rounded-md focus:border-orange-500 focus:ring-1 focus:ring-orange-500 outline-none"
+              className="w-full pl-10 pr-4 py-2 border rounded-3xl focus:border-orange-500 focus:ring-1 focus:ring-orange-500 outline-none"
             />
           </div>
         </div>
 
         {/* User actions */}
-        <div className="flex items-center gap-2 md:gap-4">
+        <div className="flex items-center gap-2 md:gap-4 ">
           <div className="flex">
-            <CiBitcoin className="text-yellow-700 text-2xl " />
+            <Bitcoin className="text-yellow-700 text-2xl " />
             <div className="text-yellow-700">
               {Number(userBalance).toFixed()} CRC
             </div>
           </div>
           {/* User Icon */}
-          <FaUserCircle
-            className="hidden md:flex text-gray-600 text-2xl cursor-pointer hover:ring-2 hover:ring-yellow-700 hover:rounded-xl"
+          <CircleUserRound
+            size={30}
+            className="hidden md:flex text-orange-600 text-2xl md:text-4xl cursor-pointer hover:ring-2 hover:ring-yellow-700 hover:rounded-3xl"
             onClick={() => router.push("/profile/i")}
           />
 
           {/* Mobile menu button */}
           <div className="md:hidden">
-            <FaBars
+            <Menu
               className="text-gray-600 text-2xl cursor-pointer"
               onClick={toggleDropdown}
             />
           </div>
 
           {/* Become a creator button (hidden on mobile) */}
-          <button
-            onClick={() => router.push("/profile/creator")}
-            className="hidden md:flex bg-orange-500 text-white px-4 py-2 rounded-md hover:ring-2 hover:ring-orange-700 "
-          >
-            Become a creator
-          </button>
+          {!isUserCreator && (
+            <button
+              onClick={() => router.push("/profile/creator")}
+              className="hidden md:flex bg-orange-500 text-white px-4 py-2 rounded-md hover:ring-2 hover:ring-orange-700 "
+            >
+              Become a creator
+            </button>
+          )}
         </div>
       </div>
 
